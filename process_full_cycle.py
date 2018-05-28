b6be7cad6cb1b50fef1aa6c591cb91adca38f553
MapleS <sxm728@dlut.edu.cn>
2018-05-28 10:28:19 +0800

    full cycle: MSE 5.9W

--- process_full_cycle.py
+++ process_full_cycle.py
@@ -18,10 +18,10 @@ lr = 0.01
 
 output_size = 1
 
-batch_size = 24
+batch_size = 100
 
 keep_prob = 0.75
-cycle_timesteps = 3
+cycle_timesteps = 4
 
 is_train = False
         
@@ -29,10 +29,10 @@ is_train = False
 X_ = tf.placeholder(tf.float32, [None, cycle_timesteps])
 y = tf.placeholder(tf.float32, [None, output_size])
 
-w1_in = tf.Variable(tf.truncated_normal([cycle_timesteps, 12]))
-b1_in = tf.Variable(tf.constant(0.1, shape = [12], dtype = tf.float32))
+w1_in = tf.Variable(tf.truncated_normal([cycle_timesteps, 6]))
+b1_in = tf.Variable(tf.constant(0.1, shape = [6], dtype = tf.float32))
 
-w2_in = tf.Variable(tf.truncated_normal([12, 1]))
+w2_in = tf.Variable(tf.truncated_normal([6, 1]))
 b2_in = tf.Variable(tf.constant(0.1, shape = [1], dtype = tf.float32))
  
 
@@ -46,7 +46,7 @@ train_op = tf.train.AdamOptimizer(lr).minimize(loss_func)
 records = pd.read_csv(data_path)
 # normalization
 scaler = MinMaxScaler()
-records['power'] = scaler.fit_transform(records['power'])
+records['power'] = scaler.fit_transform( records['power'].reshape((-1,1))).reshape((-1))
     
 train_records = records.iloc[: len(records) - 24 * 60]
 test_records = records.iloc[len(records) - 24 * 60 : ]
@@ -62,53 +62,64 @@ def get_cycle_time_batch_data(records, batch_sz, time_step):
         cur = records.iloc[i]
         cycle_x.append( records[:i][(records['hour'] == cur['hour'])]['power'][ -cycle_timesteps :] )
         y.append(cur['power'])
-
+        
         if len(y) == batch_sz:
             yield np.reshape(cycle_x, (-1, cycle_timesteps)), np.reshape(y, (-1, output_size))
             # reset data
             series_x = []
             cycle_x = []
             y = []
-            
-def predict_model(display = False):
 
+    if len(y) > 0:
+        yield np.reshape(cycle_x, (-1, cycle_timesteps)), np.reshape(y, (-1, output_size))
+            
+def predict_model(display = False, sess = None):
+    
+
+    # just using test records
+    records = test_records
     saver = tf.train.Saver()
-    with tf.Session() as sess:
+
+    if sess is None:
+        sess = tf.Session()
         # restore model
         saver.restore(sess, model_path)
-        test_data = get_cycle_time_batch_data(records, batch_size, cycle_timesteps)
-        test_y_list = [] 
-        test_y_pre_list = []
-        test_all_loss = []
-        for batch in test_data:
-            predict, loss = sess.run([y_pre, loss_func], feed_dict = {X_ : batch[0], y : batch[1]})
-            test_y_list.extend(batch[1])
-            test_y_pre_list.extend(predict)
-            test_all_loss.append(loss)
+    test_data = get_cycle_time_batch_data(records, batch_size, cycle_timesteps)
+    test_y_list = [] 
+    test_y_pre_list = []
+    test_all_loss = []
+    for batch in test_data:
+        predict, loss = sess.run([y_pre, loss_func], feed_dict = {X_ : batch[0], y : batch[1]})
+        test_y_list.extend(batch[1])
+        test_y_pre_list.extend(predict)
+        test_all_loss.append(loss)
 
-        # display
-        test_x = list(range(len(test_y_list)))
+    # display
+    test_x = list(range(len(test_y_list)))
 
-        # inverse normalization
-        test_y_list = scaler.inverse_transform(test_y_list)
-        test_y_pre_list = scaler.inverse_transform(test_y_pre_list)
-        test_y_list = np.array(test_y_list)
-        test_y_pre_list = np.array(test_y_pre_list)
+    # inverse normalization
+    test_y_list = scaler.inverse_transform(test_y_list)
+    test_y_pre_list = scaler.inverse_transform(test_y_pre_list)
+    test_y_list = np.array(test_y_list)
+    test_y_pre_list = np.array(test_y_pre_list)
 
-        mse = np.mean( (test_y_list - test_y_pre_list) ** 2)
+    mse = np.mean( (test_y_list - test_y_pre_list) ** 2)
+    if display:
         print('---------------- Test Loss:', np.mean(test_all_loss), 'MSE:', mse)
-        if display:
-            plt.plot(test_x, test_y_list, 'r', test_x, test_y_pre_list, 'b')
-            plt.show()
+        plt.plot(test_x, test_y_list, 'r', test_x, test_y_pre_list, 'b')
+        plt.show()
+    else:
+        return mse
 
 # ----------------- Train ----------
 def train():
     plt.figure(1)
 
     with tf.Session() as sess:
-        min_loss = 100000
+        min_test_loss = 1000000000
+        
         saver = tf.train.Saver()
-        for i in range(10000):
+        for i in range(1000000000):
             y_list = [] 
             y_pre_list = []
             all_loss = []
@@ -124,15 +135,15 @@ def train():
                 y_pre_list.extend(predict)
                 all_loss.append(loss)
 
-            m_loss = np.mean(all_loss)
-            print('Epoch', i, 'Loss', m_loss)
+            print('Epoch', i, 'Loss', np.mean(all_loss))
+
+            m_loss = predict_model(sess = sess)
             
-            if m_loss < min_loss:
-                min_loss = m_loss
+            if m_loss < min_test_loss:
+                min_test_loss = m_loss
                 print('--------------- Save model:', m_loss)
                 # save model
                 saver.save(sess, model_path)
-                predict_model()
 
 if is_train:
     train()
